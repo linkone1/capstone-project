61d1a8504c3c6f91de43db6bf26d6f36c648954a
Linkan333 <linunybe@gmail.com>
2025-01-05 19:31:13 +0100

    10. Withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent)  {
         feeAccount = _feeAccount;
@@ -23,7 +24,7 @@ contract Exchange {
 
     function depositToken(address _token, uint256 _amount) public {
         
-        // Transfer tokens to exchange added Require() to add a second layer of extra security.
+        // Transfer tokens to exchange added Require() to add a second layer of extra confirmation of approval.
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         
         // Update user balance
@@ -31,9 +32,20 @@ contract Exchange {
         
         // Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]); 
+    }
 
+    function withdrawToken(address _token, uint256 _amount) public {
+        // Ensure user has enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);
 
+        // Transfer tokens to user
+        Token(_token).transfer(msg.sender, _amount);
 
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        // Emit event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     // Check balances
