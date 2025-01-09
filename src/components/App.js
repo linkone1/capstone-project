78794ceffed5a055c956fdbf5b99d17b5716da66
Linkan333 <linunybe@gmail.com>
2025-01-09 12:29:15 +0100

    Added Markets dropdown menu so you can change between CTK / mETH, CTK / mDAI

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import {
 } from '../store/interactions';
 
 import Navbar from './Navbar';
+import Markets from './Markets';
 
 
 function App() {
@@ -60,7 +61,7 @@ function App() {
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+          <Markets />
 
           {/* Balance */}
 
