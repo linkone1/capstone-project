123ad262ab8705bbc7537e49b67f57b2a0d85189
Linkan333 <linunybe@gmail.com>
2025-01-12 19:02:44 +0100

    Filling Orders with button click now

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -1,11 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import sort from '../assets/sort.svg';
 import { orderBookSelector } from '../store/selectors';
+import { fillOrder } from '../store/interactions';
 
 const OrderBook = () => {
+    const provider = useSelector(state => state.provider.connection);
+    const exchange = useSelector(state => state.exchange.contract);
+    const dispatch = useDispatch();
     const symbols = useSelector(state => state.tokens.symbols);
     const orderBook = useSelector(orderBookSelector);
+
+    const fillOrderHandler = (order) => {
+        fillOrder(provider, exchange, order, dispatch)
+    }
 
     return (
       <div className="component exchange__orderbook">
@@ -33,7 +41,7 @@ const OrderBook = () => {
 
                     {orderBook && orderBook.sellOrders.map((order, index) => {
                         return (
-                         <tr key={index}>
+                         <tr key={index} onClick={() => fillOrderHandler(order)}>
                             <td>{order.token0Amount}</td>
                             <td style={{ color: `${order.orderTypeClass}` }}>{order.tokenPrice}</td>
                             <td>{order.token1Amount}</td>
@@ -69,7 +77,7 @@ const OrderBook = () => {
 
                 {orderBook && orderBook.buyOrders.map((order, index) => {
                     return (
-                    <tr key={index}>
+                    <tr key={index} onClick={() => fillOrderHandler(order)}>
                         <td>{order.token0Amount}</td>
                         <td style={{ color: `${order.orderTypeClass}` }}>{order.tokenPrice}</td>
                         <td>{order.token1Amount}</td>
